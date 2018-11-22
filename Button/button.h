c16e3648f9ea53b9ff2b61d9e3a7fd5ce0a16baf
RaggedyAnn <rikignirrak@hotmail.com>
2018-11-22 15:02:01 +0100

    Is button on or off

---
+++ Button/button.h
@@ -0,0 +1,17 @@
+boolean ButtonIsDown(int name) {
+  int buttonState = digitalRead(name);
+  if (buttonState == HIGH) {
+    return true;
+  } else {
+    return false;
+  } 
+}
+
+boolean isButtonUp(int name) {
+  int buttonState = digitalRead(name);
+  if (buttonState == HIGH) {
+    return false;
+  } else {
+    return true;
+  } 
+}
