ae346416fae42a45202361aedf9b1cb546a977ca
RaggedyAnn <rikignirrak@hotmail.com>
2018-11-23 12:50:53 +0100

    Create Flex.h

---
+++ Flex/Flex.h
@@ -0,0 +1,26 @@
+float caliZero = 0;
+
+int getValue(int name) {
+  int val = analogRead(name);
+  Serial.println(val);
+  int mapped = map(val, 0,1023,0,100);
+  return mapped;
+}
+
+boolean isBend(int name) {
+  int val = analogRead(name);
+  if(val > caliZero + 30 || val < caliZero - 30) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+void calibrate(int name) {
+  int one = analogRead(name);
+  delay(5);
+  int two = analogRead(name);
+  delay(5);
+  int three = analogRead(name);
+  caliZero = (one+two+three) / 3;
+}
