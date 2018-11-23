63f1c8faf0495a8be14a52e378cd2f7e79c2f09f
RaggedyAnn <rikignirrak@hotmail.com>
2018-11-23 13:41:58 +0100

    Update general.h

--- functionLibrary/general.h
+++ functionLibrary/general.h
@@ -4,3 +4,12 @@ int getValue(int name) {
   int mapped = map(val, 0,1023,0,100);
   return mapped;
 }
+
+void calibrate(int name) {
+  int one = analogRead(name);
+  delay(5);
+  int two = analogRead(name);
+  delay(5);
+  int three = analogRead(name);
+  caliZero = (one+two+three) / 3;
+}
