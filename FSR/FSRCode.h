1064be5486686b86ca6ee31a7f986fc6a6a86eae
RaggedyAnn <rikignirrak@hotmail.com>
2018-11-23 13:55:54 +0100

    Update FSRCode.h

--- FSR/FSRCode.h
+++ FSR/FSRCode.h
@@ -1,9 +1,3 @@
-int getValue(int name) {
-  int val = analogRead(name);
-  int mapped = map(val, 0,1023,0,100);
-  return mapped;
-}
-
 boolean isTouched(int name) {
   int val = analogRead(name);
   if(val > 10) {
