9c65883c06b7139a7560e3b504317c3a1d8e5517
RaggedyAnn <rikignirrak@hotmail.com>
2018-11-28 22:23:58 +0100

    add comments

--- Flex/Flex.ino
+++ Flex/Flex.ino
@@ -1,16 +1,22 @@
 #include "flexCode.h"
 
+//Set the pin which is connected to the flex sensor
 const int flexPin = 2;
 int value;
 
 void setup() {
+  //calibrate the sensor to avoid inaccuracy
+  //NB! This can only be done to one of your inputs
   calibrate(flexPin);
 }
 
 void loop() {
  if(isBend(flexPin)) {
+  //write what happens if the sensor is bent
+  //getValue reads the amout the sensor is bent (as a number between 0 (not bent) and 100 (fully bent))
   value = getValue(flexPin);
  } else {
+    //write what happens if the sensor is not bent
     value = 0;
  }
 }
