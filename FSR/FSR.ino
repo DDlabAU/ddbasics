8775791d942cfa30553f65d01bd6515887a60f2f
RaggedyAnn <rikignirrak@hotmail.com>
2018-11-28 22:21:16 +0100

    add comments

--- FSR/FSR.ino
+++ FSR/FSR.ino
@@ -1,16 +1,22 @@
 #include "FSRCode.h"
 
+//Set the pin which the force sensor is connected to
 const int fsrPin = 4;
 int value = 0;
 
 void setup() {
+  //Calibrate the sensor at the begining of the program to avoid inaccuracy 
+  //NB! This can only be done to one of your inputs
   calibrate(fsrPin);
 }
 
 void loop() {
   if (isTouched(fsrPin)) {
+    //write what happens if the sensor is touched
+    //getValue reads the force put upon the sensor
     value = getValue(fsrPin);
   } else {
+    //write what happens if the sensor is not touched
     value = 0;
   }
 }
