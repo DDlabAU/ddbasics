d668e03d422f19a5c6d426694bac3198680c02eb
RaggedyAnn <rikignirrak@hotmail.com>
2018-11-28 22:28:26 +0100

    comments added

--- LED/LED.ino
+++ LED/LED.ino
@@ -8,7 +8,10 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //ledOn has 3 parameters: 
+  //1st: the number of the LED you want to light, 
+  //2nd: the color toy want the LED, 
+  //3rd: the brightness of the LED indicated as a number between 0 (no light) and 100 (max brightness)
   ledOn(1,green, 50);
   ledOn(2,white, 10);
   pause(1.5);
