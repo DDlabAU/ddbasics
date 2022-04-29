f285fa9b20e6a2fa67159878a68e238d1c8a0586
NonhumAnn <rikignirrak@hotmail.com>
2022-04-29 10:55:34 +0200

    UpdateMap

--- DDpiezo.h
+++ DDpiezo.h
@@ -1,6 +1,14 @@
 int mappedFreq;
 
 void play(int pin, int frequency) {
-  mappedFreq = int(map(0,100,261,1046));
+  mappedFreq = map(frequency,0,100,261,1046);
   tone(pin, mappedFreq);
+}
+
+void highestNote(int pin){
+  tone(pin, 1046);
+}
+
+void lowestNote(int pin) {
+  tone(pin,261);
 }
